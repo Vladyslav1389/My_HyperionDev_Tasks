c0d730b674b7aa74c4590e800c4a9421bb72b6b4
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-01-20 19:47:44 +0000

    Create all list and dictionaries

---
+++ T12 List and Dictionary/cafe.py
@@ -0,0 +1,16 @@
+"""
+The program counts the cafe's total stock value. 
+"""
+
+"""
+1. Creating a menu list.
+2. Creating the stock value for each item on the menu.
+3. Creating a price for each item on the menu.
+4. Calculating total stock worth in the cafe.
+5. Printing the result.
+"""
+
+menu = ["Tea", "Beer", "Cider", "Coffe"]
+stock = {'Tea': 13, 'Beer': 50, 'Cider': 33, 'Coffe': 56}
+price = {'Tea': 3.50, 'Beer': 5.90, 'Cider': 5.30, 'Coffe': 4.10}
+
