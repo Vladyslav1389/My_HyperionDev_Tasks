ce9fdd61b334b349e7ce44aced41c6b7b6670771
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-01-21 20:08:19 +0000

    commit whole program

--- T12 List and Dictionary/cafe.py
+++ T12 List and Dictionary/cafe.py
@@ -1,16 +1,17 @@
 """
-The program counts the cafe's total stock value. 
+The program counts the cafe's total stock worth. 
 """
 
-"""
-1. Creating a menu list.
-2. Creating the stock value for each item on the menu.
-3. Creating a price for each item on the menu.
-4. Calculating total stock worth in the cafe.
-5. Printing the result.
-"""
-
+# Entering all data
 menu = ["Tea", "Beer", "Cider", "Coffe"]
 stock = {'Tea': 13, 'Beer': 50, 'Cider': 33, 'Coffe': 56}
 price = {'Tea': 3.50, 'Beer': 5.90, 'Cider': 5.30, 'Coffe': 4.10}
 
+# Creating a variable in which will be calculated total stock worth
+total_stock = 0
+
+# Itereting through the menu list and using each of its items as a key in the dictionaries and calculating the total stock worth
+for item in menu:
+    each_item_value = stock[item] * price[item]
+    total_stock += each_item_value
+print(f"The total stock worth in the cafe is {total_stock} pounds")
