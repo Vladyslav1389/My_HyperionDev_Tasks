4104188127cc2fe29389fcbb4d39363a66e4c8b9
Vladyslav Shcherbak <vladserbak890@gmail.com>
2024-02-29 21:31:05 +0000

    Added task completion check

--- T17 - Capstone Project - Lists, Functions, and String Handling/task_manager copy.py
+++ T17 - Capstone Project - Lists, Functions, and String Handling/task_manager copy.py
@@ -152,27 +152,31 @@ e - Exit
                     print(f"{task_list[int(user_task_choice) - 1]}")
  ###==========================================================================               
                 if mark_or_edit_choice == 'e':
-                    edit_task_choice = validate_username("Please enter 'n' if you want to change "
-                                                         "the username of the person to whom the task "
-                                                         "is assigned\nor 'd' if you want to change the"
-                                                         " due date of the task\nor any other button to choose another task: ")
-                    
-                    if edit_task_choice == 'n':
-                        print(f"All usernames of people: ", end='')
-                        print(', '.join(username_password.keys()))
-                        while True:
-                            new_assigned_user = validate_username("Please enter the name of the person"
-                                                              " to whom you want to assign the task to: ")
-                            if new_assigned_user in username_password.keys():
-                                task_list[int(user_task_choice) - 1]['username'] = new_assigned_user
-                                write_tasks_to_file(task_list, path_tasks_txt)
-                                break
-                            else:
-                                print("Unexist person or incorrect person name!")
-                    
-                    if edit_task_choice == 'd':
-                        task_list[int(user_task_choice) - 1]['due_date'] = date_validation()
-                        write_tasks_to_file(task_list, path_tasks_txt)
+                    if task_list[int(user_task_choice) - 1]['completed'] == False:
+
+                        edit_task_choice = validate_username("Please enter 'n' if you want to change "
+                                                            "the username of the person to whom the task "
+                                                            "is assigned\nor 'd' if you want to change the"
+                                                            " due date of the task\nor any other button to choose another task: ")
+                        
+                        if edit_task_choice == 'n':
+                            print(f"All usernames of people: ", end='')
+                            print(', '.join(username_password.keys()))
+                            while True:
+                                new_assigned_user = validate_username("Please enter the name of the person"
+                                                                " to whom you want to assign the task to: ")
+                                if new_assigned_user in username_password.keys():
+                                    task_list[int(user_task_choice) - 1]['username'] = new_assigned_user
+                                    write_tasks_to_file(task_list, path_tasks_txt)
+                                    break
+                                else:
+                                    print("Unexist person or incorrect person name!")
+                        
+                        if edit_task_choice == 'd':
+                            task_list[int(user_task_choice) - 1]['due_date'] = date_validation()
+                            write_tasks_to_file(task_list, path_tasks_txt)
+                    else:
+                        print("This task is already completed.")
 
             else:
                 print("You entered task that do not assigned to particular user.")
